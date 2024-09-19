d128f98a6ae2c9f73b466b3dd032e77aa37bae74
Spoty-O <korotysh16@ukr.net>
2024-09-19 19:29:03 +0300

    work

--- server.py
+++ server.py
@@ -3,34 +3,27 @@ from main_db_controll import db
 import os
 
 UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
 
 def allowed_file(filename):
    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def index():
-   return render_template('main.html')
-   # if request.method == "POST":
-   #    info = request.form.to_dict(flat=False)
-   #    db.add_data(info)
-   #    data = db.get_data()
-   #    return render_template('answer.html', obj=data)
+   data = db.get_data()
+   return render_template('main.html', data=data)
    
-def answer():
+def add():
    file = request.files['file']
    if file and allowed_file(file.filename):
       # Сохранение файла в указанной папке
       file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
    form_data = tuple(request.form.values()) + (file.filename,)
-   print(form_data)
    db.add_data(form_data)
-   data = db.get_data()
-   print(data)
-   return render_template('answer.html', obj=data)
+   return redirect(url_for('index'))
 
 app = Flask(__name__)
 app.add_url_rule('/', 'index', index, methods=["GET"])
-app.add_url_rule('/answer', 'answer', answer, methods=["POST"])
+app.add_url_rule('/add', 'add', add, methods=["POST"])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 if __name__ == '__main__':
    if not os.path.exists(UPLOAD_FOLDER):
